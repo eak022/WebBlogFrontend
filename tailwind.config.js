9c2bbb649fad41e9a459f8e421cf4bd69fa79519
eak022 <eakeakkachai71180@gmail.com>
2025-01-03 14:55:01 +0700

    03

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   darkMode: 'class', // เพิ่มการตั้งค่านี้
 };
