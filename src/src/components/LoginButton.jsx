80fb4beb4ca9ce9b3494e42934985e25774fea30
eak022 <121035260+eak022@users.noreply.github.com>
2025-01-12 02:27:51 +0700

    Delete src/src directory

--- src/src/components/LoginButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoginButton = () => {
-  <button className="btn btn-outline btn-accent">Login</button>;
-};
-
-export default LoginButton;
--- src/src/components/RegisterButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RegisterButton = () => {
-  <button className="btn btn-outline btn-primary">Register</button>;
-};
-
-export default RegisterButton;
