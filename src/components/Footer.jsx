237756948646cfae798c826c2d3f3efc2f09270c
eak022 <121035260+eak022@users.noreply.github.com>
2025-01-12 14:37:47 +0700

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div></div>
+    <footer className="footer footer-center bg-base-300 text-base-content p-4 mt-auto">
+    <aside>
+      <p> © {new Date().getFullYear()} Software Engineering, NPRU. All rights reserved.</p>
+    </aside>
+  </footer>
   )
 }
 
